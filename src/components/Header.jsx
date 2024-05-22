e01de351dcd770643895be827cf06ec26feec2d6
student-selflearenr <gunisu3@gmail.com>
2024-05-22 19:18:44 +0200

    welcome screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,7 @@
 const Header = () => {
   return (
       <div className="relative w-full h-72 bg-cover bg-center rounded-lg overflow-hidden">
-          <img
-              src=".\src\assets\header1.png"
-              alt="Header background"
-              className="absolute w-full h-full object-cover"
-          />
+          <img src=".\src\assets\header1.png" alt="Header background" className="absolute w-full h-full object-cover" />
           <div className="absolute w-full h-full flex justify-center items-center bg-black bg-opacity-5">
               <h1 className="text-black font-bold text-3xl md:text-5xl p-4 my-4">
                   Aconsegueix la millor qualitat
