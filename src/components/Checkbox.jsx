7117d189d0a7b8ef192af7c1bd0a2d7f0f86d958
student-selflearenr <gunisu3@gmail.com>
2024-05-27 12:52:30 +0200

    Ejercicio-9

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -17,20 +17,21 @@ const Checkbox = () => {
     const [pages, setPages] = useState(0);
     const [languages, setLanguages] = useState(0);
     const [budgets, setBudgets] = useState([]);
-    const [webPopUp , setWebPopUp] = useState(false);
+    const [webPopUp, setWebPopUp] = useState(false);
+    const [isAnnual, setIsAnnual] = useState(false);
 
 
     const handleChange = (option) => {
         setOptions(prevOptions => {
             const newOption = {
-            ...prevOptions,
-            [option]: !prevOptions[option]
+                ...prevOptions,
+                [option]: !prevOptions[option]
             }
 
             //for popup
 
 
-            if(option === 'web' && !prevOptions.web){
+            if (option === 'web' && !prevOptions.web) {
                 setWebPopUp(true);
             }
             return newOption;
@@ -47,51 +48,82 @@ const Checkbox = () => {
 
     const handleDecrement = (field) => {
         if (field === 'pages') {
-                setPages((prevPages => (prevPages > 0 ? prevPages - 1 : 0)))
-            
+            setPages((prevPages => (prevPages > 0 ? prevPages - 1 : 0)))
+
         } else if (field === 'languages') {
-                setLanguages(prevLanguages => (prevLanguages > 0 ? prevLanguages - 1 : 0));
-            }
+            setLanguages(prevLanguages => (prevLanguages > 0 ? prevLanguages - 1 : 0));
         }
-    
+    }
+
 
     const calculateTotal = () => {
         let total = services.reduce((total, service) => {
             return options[service.title.toLowerCase()] ? total + service.price : total;
         }, 0);
-        
+
         if (options.web) {
             total += (pages + languages) * 30
         }
+
+
+        //update for discount
+
+        if(isAnnual){
+            total*= 0.8
+        }
+
+
         return total;
     };
 
     const total = calculateTotal();
 
     const addBudget = (budgetDetails) => {
-      const newBudget = {
-        ...budgetDetails,
-        services: Object.keys(options).filter(option => options[option]),
-        total: total,
-        date:new Date().toString(),
-        id: budgets.length + 1,
-      };
-      setBudgets((prevBudgets) => [...prevBudgets, newBudget]);
+        const newBudget = {
+            ...budgetDetails,
+            services: Object.keys(options).filter(option => options[option]),
+            total: total,
+            date: new Date().toString(),
+            id: budgets.length + 1,
+        };
+        setBudgets((prevBudgets) => [...prevBudgets, newBudget]);
     };
 
+    const handleToggleAnnual = () => {
+        setIsAnnual(!isAnnual);
+    }
     return (
         <div className="container mx-auto p-6 relative">
             <br />
 
-        <Link to="/" className="absolute top-3 right-3">
-            
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-5">
-                <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
-                <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
-            </svg>
-        </Link>
+            <Link to="/" className="absolute top-3 right-3">
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-5">
+                    <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                    <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+                </svg>
+            </Link>
 
             <Header />
+
+            <br />
+            <br />
+
+            <div className="flex justify-center">
+
+                <label className="label cursor-pointer gap-5">
+
+                    <span className="font-bold fint-snas text-2xl">Pagament mensual</span>
+
+                    <div className="realtive inline-block -10 mr-2 align-middle select-none transition duratioin-200 ease-in" >
+                        <input type="checkbox" className="toggle" checked={isAnnual} onChange={handleToggleAnnual} ></input>
+                    </div>
+
+                    <span className="font-bold fint-snas text-2xl">Pagament anual</span>
+
+                </label>
+
+            </div>
 
             {services.map((service) => (
                 <div key={service.id} className="mx-auto md:w-5/6 shadow-xl md:p-10 rounded-3xl my-8 border duration-500">
@@ -108,12 +140,12 @@ const Checkbox = () => {
 
                     <label className="label cursor-pointer justify-end gap-2">
                         <input
-                            className="checkbox border-emerald-700 [--chkbg:theme(colors.emerald.700)]"   type="checkbox" id={service.title.toLowerCase()} onChange={() => handleChange(service.title.toLowerCase())}/>
+                            className="checkbox border-emerald-700 [--chkbg:theme(colors.emerald.700)]" type="checkbox" id={service.title.toLowerCase()} onChange={() => handleChange(service.title.toLowerCase())} />
                         <span className="font-semibold text-xl">Afegir</span>
                     </label>
 
                     <br />
-                    
+
                     {service.title.toLowerCase() === "web" && options.web && (
                         <WebsiteData pages={pages} languages={languages} setPages={setPages} setLanguages={setLanguages} handleDecrement={handleDecrement} handleIncrement={handleIncrement} />
                     )}
@@ -122,34 +154,34 @@ const Checkbox = () => {
             ))}
 
             <footer className="flex justify-between font-bold mt-6">
-                <TotalPrice total={total} />
+                <TotalPrice total={total} isAnnual={isAnnual}/>
 
                 <Link to='/'><button className=" font-serif btn btn-outline btn-success">Home</button></Link>
-                
+
             </footer>
 
-          <BudgetForm addBudget={addBudget}/>
-          <BudgetList budgets={budgets}/>
+            <BudgetForm addBudget={addBudget} />
+            <BudgetList budgets={budgets} />
 
-          {webPopUp && (
+            {webPopUp && (
 
-            <div className="fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50 flex justify-center items-center">
+                <div className="fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50 flex justify-center items-center">
 
-                <div className="bg-white font-serif border rounded p-4 w-1/3">
+                    <div className="bg-white font-serif border rounded p-4 w-1/3">
 
-                    <h2 className="text-lg font-bold text-center mb-2">Número de Paginas y llenguatges</h2>
-                    <br />
+                        <h2 className="text-lg font-bold text-center mb-2">Número de Paginas y llenguatges</h2>
+                        <br />
 
-                    <p>Afegeix les llenguatges que tindrà el teu projecte. El cost de cada llenguatge és de 30€.</p>
+                        <p>Afegeix les llenguatges que tindrà el teu projecte. El cost de cada llenguatge és de 30€.</p>
 
-                    <br/>
+                        <br />
 
-                    <button className="bg-gray-100 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow" onClick={() => setWebPopUp(false)} >Close</button>
+                        <button className="bg-gray-100 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow" onClick={() => setWebPopUp(false)} >Close</button>
+
+                    </div>
 
                 </div>
-
-            </div>
-          )}
+            )}
 
         </div>
     );
--- src/components/TotalPrice.jsx
+++ src/components/TotalPrice.jsx
@@ -1,13 +1,15 @@
 
-const TotalPrice = ({total}) => {
+const TotalPrice = ({total ,isAnnual}) => {
     return (
         <div className="md:w-5/6 mx-auto flex md:flex-row flex-col justify-center md:justify-end md:gap-12 my-20 items-center md:items-baseline">
             <p className="md:text-start text-center text-4xl font-bold md:mb-0 mb-6">
                 Preu pressuposat:
             </p>
             <p className="text-5xl font-extrabold md:ms-20">
-                {total} €
+            <span>Total: {total.toFixed(2)} €</span>
+            {isAnnual && <span className="text-red-500 text-lg"> (Now 20% discount)</span>}
             </p>
+
         </div>
     );
 };
